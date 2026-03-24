e7984398788711a64e4d23202c9c38c416532fbf
dirtycowteet <crasor@pdx.edu>
2026-03-23 21:26:11 -0700

    added adc_dt sample along with appropriate overlay for c092 board

--- flash.ps1
+++ flash.ps1
@@ -2,9 +2,12 @@
 
 $projectname = 'zephyrproject'
 $board = 'nucleo_c092rc'
-$app = 'sensor_shell' 
+$app = 'adc\adc_dt' 
+$dir = '2027-SAE-Electric-VCU'
+$path = 'C:\Users\casey\OneDrive\Desktop\ECE413'
+
 cd ~\zephyr\$projectname\
 .venv\scripts\activate.ps1
-west build -p always -b $board ..\..\OneDrive\Desktop\ECE413\VMS-VCU\$app
+west build -p always -b $board $path\$dir\$app
 west flash
-cd ~\OneDrive\Desktop\ECE413\VMS-VCU
+cd $path\$dir
